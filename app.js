53bfff4b2bdbfea93eb878ec956fb1cf0b71ea7e
Ameria Ahweera <94245206+000789Ameria@users.noreply.github.com>
2023-11-19 18:31:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ square.addEventListener('click', function(){
   }else{
     alert("Thanks for staying with us.")
   }
-});
+});<body>
+            <div id="google_translate_element"></div>
+            <script type="text/javascript">
+                function googleTranslateElementInit(){
+                    new
+                        Google.translate.TranslateElement({pageLanguage:'en'}, 'google_translate_element')}
+            </script>
+
+            <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+</head>

bf3bb0802892108ac6aeb135e30e5806245c7568
Ameria Ahweera <94245206+000789Ameria@users.noreply.github.com>
2023-11-19 17:44:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ square.addEventListener('click', function(){
     alert("Thanks for staying with us.")
   }
 });
+function changeLang() {
+    var language = getCookie("lang");
+    if (test == "en") {
+        setCookie("lang", "es", 365);
+        window.location.reload();
+    } else {
+        setCookie("lang", "en", 365);
+        window.location.reload();
+    }    
+}

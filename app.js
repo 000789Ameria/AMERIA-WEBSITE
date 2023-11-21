5de41a870064674ba1a16de78517a3ad8b9b2621
Ameria Ahweera <94245206+000789Ameria@users.noreply.github.com>
2023-11-21 21:49:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const square = document.getElementById('square');
-square.addEventListener('click', function(){
-  var result = confirm("Do you want to exit page?")
-  if(result==true){
-    alert("Thanks for visiting my page.")
-  }else{
-    alert("Thanks for staying with us.")
-  }
+function changeLang() {
+    var language = getCookie("lang");
+    if (test == "en") {
+        setCookie("lang", "es", 365);
+        window.location.reload();
+    } else {
+        setCookie("lang", "en", 365);
+        window.location.reload();
+    }    
+}
+  
 

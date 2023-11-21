3a99ec4b196d6c9e8c9e6af9e65c883aa1d5e6b2
Ameria Ahweera <94245206+000789Ameria@users.noreply.github.com>
2023-11-21 22:11:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-document.getElementById('btn') = function changeLang() {
-    var language = getCookie("lang");
-    if (test == "en") {
-        setCookie("lang", "es", 365);
-        window.location.reload();
-    } else {
-        setCookie("lang", "en", 365);
-        window.location.reload();
-    }    
-}
+
   
 
